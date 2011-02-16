478d3206e492bba5b4b5b0badf48e889473ded74
Tom Dineen <dineen5214@gmail.com>
2011-02-16 17:44:11 -0500

    Pretty much final tested

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,10 +4,6 @@ class MicropostsController < ApplicationController
   def index
     @microposts = Micropost.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @microposts }
-    end
   end
 
   # GET /microposts/1
